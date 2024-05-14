d4761a090d4a6b0300287671d3b0e94d9123a946
RohitRaj766 <rohitraj2k04@gmail.com>
2024-05-14 13:14:52 +0530

    .

---
+++ ass-2/q1.ts
@@ -0,0 +1,7 @@
+// const divideByTwo = (source: number): number => {
+//     return source / 2;
+// };
+
+// const result: number = divideByTwo(10);
+
+// console.log(result); 
---
+++ ass-2/q2.ts
@@ -0,0 +1,7 @@
+// const checkVowel = (data: string): boolean => {
+//     const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+//     return vowels.includes(data);
+// };
+// const result: boolean = checkVowel('A');
+
+// console.log(result); 
